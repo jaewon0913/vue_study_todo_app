39ea5e6ed208169ae4f010b87fdc0a50a022fa3a
jaewon0913 <jaewon9937@gmail.com>
2021-05-22 15:44:43 +0900

    const와 let 변경

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ export default {
   },
   methods: {
     addOneItem: function(todoItem){
-      var obj = {completed: false, item: todoItem};
+      const obj = {completed: false, item: todoItem};
       localStorage.setItem(todoItem, JSON.stringify(obj));
       this.todoItems.push(obj);
     },
@@ -56,7 +56,7 @@ export default {
   created: function(){
     //console.log('created');
     if (localStorage.length > 0){
-      for (var i = 0; i < localStorage.length; i ++){
+      for (let i = 0; i < localStorage.length; i ++){
         if (localStorage.key(i) !== 'loglevel:webpack-dev-server'){
           this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
           //this.todoItems.push(localStorage.key(i));
