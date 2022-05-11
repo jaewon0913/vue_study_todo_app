b9dd6dcb94aca60fe4cb8e77b6fe0276d56aca6e
jaewon0913 <jaewon9937@gmail.com>
2022-05-11 15:01:17 +0900

    [기능추가] Vuex - 프로젝트 구조화 및 모듈화 - 스토어 속성 모듈화 방법

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import * as getters from './getters'
+import * as mutations from './mutations'
+
 Vue.use(Vuex);
 
 const storage = {
@@ -21,30 +24,6 @@ export const store = new Vuex.Store({
     state: {
         todoItems: storage.fetch()
     },
-    getters: {
-        storedTodoItems(state) {
-            return state.todoItems;
-        }
-    },
-    mutations: {
-        addOnItems(state, todoItem){
-            const obj = {completed: false, item: todoItem};
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeOneItem(state, payload){
-            localStorage.removeItem(payload.todoItem.item);
-            state.todoItems.splice(payload.index, 1);
-        },
-        toggleOneItem(state, payload){
-            //todoItem.completed = !todoItem.completed;
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-            localStorage.removeItem(payload.todoItem.item);
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-        },
-        clearAllItems(state){
-            localStorage.clear();
-            state.todoItems = [];
-        }
-    }
+    getters: getters ,
+    mutations: mutations
 });
